9ba8f93ad79cb41ffb1457947446dbc43157a035
CDWEN0526 <58065939+CDWEN0526@users.noreply.github.com>
2023-07-03 14:49:56 +0800

    Update ChatGpt_GUI.py

--- ChatGpt_GUI.py
+++ ChatGpt_GUI.py
@@ -3,94 +3,30 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QTextBrowser, QP
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt, QThreadPool, QRunnable, pyqtSlot
 import markdown
-import os
 import requests
-import re
-import base64
+import json
+
+
 
 #api获取
 def create_completion(messages: list):
-    def get_nonce():
-        res = requests.get('https://chatgptlogin.ac/use-chatgpt-free/', headers={
-            "Referer": "https://chatgptlogin.ac/use-chatgpt-free/",
-            "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-        })
-
-        src = re.search(r'class="mwai-chat mwai-chatgpt">.*<span>Send</span></button></div></div></div> <script defer src="(.*?)">', res.text).group(1)
-        decoded_string = base64.b64decode(src.split(",")[-1]).decode('utf-8')
-        return re.search(r"let restNonce = '(.*?)';", decoded_string).group(1)
-    
-    def transform(messages: list) -> list:
-        def html_encode(string: str) -> str:
-            table = {
-                '"': '&quot;',
-                "'": '&#39;',
-                '&': '&amp;',
-                '>': '&gt;',
-                '<': '&lt;',
-                '\n': '<br>',
-                '\t': '&nbsp;&nbsp;&nbsp;&nbsp;',
-                ' ': '&nbsp;'
-            }
-            
-            for key in table:
-                string = string.replace(key, table[key])
-                
-            return string
-        
-        return [{
-            'id': os.urandom(6).hex(),
-            'role': message['role'],
-            'content': message['content'],
-            'who': 'AI: ' if message['role'] == 'assistant' else 'User: ',
-            'html': html_encode(message['content'])} for message in messages]
-    
-    headers = {
-        'authority': 'chatgptlogin.ac',
-        'accept': '*/*',
-        'accept-language': 'en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3',
-        'content-type': 'application/json',
-        'origin': 'https://chatgptlogin.ac',
-        'referer': 'https://chatgptlogin.ac/use-chatgpt-free/',
-        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
-        'x-wp-nonce': get_nonce()
+    url = 'https://chat.aivvm.com/api/chat'
+    data = {
+        "model":{"id":"gpt-3.5-turbo-16k","name":"GPT-3.5-16K"},
+        "messages":messages,
+        "key":"",
+        "prompt":"You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.",
+        "temperature":1
+        }
+    header = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0',
+        'Referer':'https://chat.aivvm.com/zh',
+        'Content-Type':'application/json',
     }
-    
-    conversation = transform(messages)
 
-    json_data = {
-        'env': 'chatbot',
-        'session': 'N/A',
-        'prompt': 'Converse as if you were an AI assistant. Be friendly, creative.',
-        'context': 'Converse as if you were an AI assistant. Be friendly, creative.',
-        'messages': conversation,
-        'newMessage': messages[-1]['content'],
-        'userName': '<div class="mwai-name-text">User:</div>',
-        'aiName': '<div class="mwai-name-text">AI:</div>',
-        'model': 'gpt-3.5-turbo',
-        'temperature': 0.8,
-        'maxTokens': 1024,
-        'maxResults': 1,
-        'apiKey': '',
-        'service': 'openai',
-        'embeddingsIndex': '',
-        'stop': '',
-        'clientId': os.urandom(6).hex()
-    }
-    try:
-        response = requests.post('https://chatgptlogin.ac/wp-json/ai-chatbot/v1/chat', 
-                             headers=headers, json=json_data)
-        print(response.text)
-        return response.json()['reply']
-    except:
-        return "请求异常，请重新点击提交。或者清理历史数据重新提问。"
-
+    data = json.dumps(data)
+    r = requests.post(url=url,data=data,headers=header)
+    return r.text
 
 #运行指定功能
 class MyRunnable(QRunnable):
@@ -110,8 +46,11 @@ class MyRunnable(QRunnable):
         # 示例：将输入的数据转换为Markdown格式的HTML
         messages.append({"role": "user", "content": data})
         response = create_completion(messages=messages)
+        bot = {'content':response,'role':'assistant'}
+        messages.append(bot)
         markdown_html = markdown.markdown(response,extensions=['markdown.extensions.extra'])
         self.submit_button.setEnabled(True)
+        print(messages)
         return markdown_html
 
 #qt5窗口
@@ -126,7 +65,7 @@ class MyWidget(QWidget):
         layout = QVBoxLayout()
 
         # 创建标题标签
-        output_label = QLabel('欢迎使用GPT-3.5 Turbo版本,本软件有持续对话的能力!\n清除历史: 将会让gpt失去对你之前提问的记忆,关闭软件也一样会清除gpt记忆。\n数据源：chatgptlogin.ac\n作者：Davin', self)
+        output_label = QLabel(f'欢迎使用GPT-3.5版本,本软件有持续对话的能力!\n清除历史: 将会让gpt失去对你之前提问的记忆,关闭软件也一样会清除gpt记忆。\n作者：Davin\n版本：{version}', self)
         output_label.setStyleSheet('color: #EEE; font-size: 12px; font-weight: bold;')
         layout.addWidget(output_label)
 
@@ -233,8 +172,10 @@ class MyWidget(QWidget):
         self.output_text.append(f'<span style="color: red;">执行：</span>【清除历史提问，会清除掉你之前与GPT的对话，GPT也不再根据上下文回答你的问题！】\n\n')
         messages.clear()
 
+
 if __name__ == '__main__':
     messages = []
+    version = "v2.2"
     app = QApplication(sys.argv)
     widget = MyWidget()
     sys.exit(app.exec_())
